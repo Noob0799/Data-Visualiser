a2f326933bd2aa75061083653a532ebd94f2c0ba
sayantan_ds <roysayantan1999@gmail.com>
2019-07-03 20:09:16 +0530

    Made pie chart data selection method

--- src/app/Components/Charts/pie/pie.component.ts
+++ src/app/Components/Charts/pie/pie.component.ts
@@ -26,7 +26,7 @@ export class PieComponent implements OnInit, OnChanges {
     }
   };
   public pieChartLabels: Label[] = [];
-  public pieChartData: number[] = [];
+  public pieChartData: number[] = [1,2,3];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [pluginDataLabels];
@@ -42,11 +42,13 @@ export class PieComponent implements OnInit, OnChanges {
   ngOnChanges() {
     if (this.pieData) {
       console.log('PieData', this.pieData);
-// tslint:disable-next-line: prefer-for-of
-      for (let i = 0; i < this.pieData[0].length; i++) {
-        this.pieChartLabels.push(this.pieData[0][i].value);
-      }
-      console.log(this.pieChartLabels);
+      if (this.pieData.length === 2) {
+        // tslint:disable-next-line: prefer-for-of
+        for (let i = 0; i < this.pieData[0].length; i++) {
+          this.pieChartLabels.push(this.pieData[0][i].value);
+        }
+        console.log(this.pieChartLabels);
+      } else if (this.pieData[0].length === 1) {}
     }
   }
 
@@ -57,28 +59,6 @@ export class PieComponent implements OnInit, OnChanges {
 
   public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
-  }
-
-  changeLabels() {
-    const words = ['hen', 'variable', 'embryo', 'instal', 'pleasant', 'physical', 'bomber', 'army', 'add', 'film',
-      'conductor', 'comfortable', 'flourish', 'establish', 'circumstance', 'chimney', 'crack', 'hall', 'energy',
-      'treat', 'window', 'shareholder', 'division', 'disk', 'temptation', 'chord', 'left', 'hospital', 'beef',
-      'patrol', 'satisfied', 'academy', 'acceptance', 'ivory', 'aquarium', 'building', 'store', 'replace', 'language',
-      'redeem', 'honest', 'intention', 'silk', 'opera', 'sleep', 'innocent', 'ignore', 'suite', 'applaud', 'funny'];
-    const randomWord = () => words[Math.trunc(Math.random() * words.length)];
-    this.pieChartLabels = Array.apply(null, { length: 3 }).map(_ => randomWord());
-  }
-
-  addSlice() {
-    this.pieChartLabels.push(['Line 1', 'Line 2', 'Line 3']);
-    this.pieChartData.push(400);
-    this.pieChartColors[0].backgroundColor.push('rgba(196,79,244,0.3)');
-  }
-
-  removeSlice() {
-    this.pieChartLabels.pop();
-    this.pieChartData.pop();
-    this.pieChartColors[0].backgroundColor.pop();
   }
 
   changeLegendPosition() {
--- src/app/Components/chart/chart.component.ts
+++ src/app/Components/chart/chart.component.ts
@@ -8,12 +8,61 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 export class ChartComponent implements OnInit, OnChanges {
 
   @Input() data;
-  constructor() { }
+  pieData;
+  barData;
+  stackData;
+  pieclicked = false;
+  barClicked = false;
+  stackclicked = false;
+  constructor() {
+   }
 
   ngOnInit() {
   }
   ngOnChanges() {
+    this.disabledButtons();
     console.log('Chart', this.data);
+    if (this.data.length === 2 || this.data[1].length === 1) {
+      (document.getElementById('pie') as HTMLInputElement).disabled = false;
+      (document.getElementById('bar') as HTMLInputElement).disabled = false;
+      this.pieSort();
+      this.barSort();
+    } else {
+      (document.getElementById('stack') as HTMLInputElement).disabled = false;
+      this.stackSort();
+    }
   }
-
+  disabledButtons() {
+    console.log(document.getElementById('pie'));
+    (document.getElementById('pie') as HTMLInputElement).disabled = true;
+    (document.getElementById('bar') as HTMLInputElement).disabled = true;
+    (document.getElementById('stack') as HTMLInputElement).disabled = true;
+  }
+  pie() {
+    console.log('Pie Clicked');
+    this.pieclicked = true;
+    this.barClicked = false;
+    this.stackclicked = false;
+  }
+  bar() {
+    console.log('Bar Clicked');
+    this.pieclicked = false;
+    this.barClicked = true;
+    this.stackclicked = false;
+  }
+  stack() {
+    console.log('Stack Clicked');
+    this.pieclicked = false;
+    this.barClicked = false;
+    this.stackclicked = true;
+  }
+  pieSort() {
+    this.pieData = this.data;
+  }
+  barSort() {
+    this.barData = this.data;
+  }
+  stackSort() {
+    this.stackData = this.data;
+  }
 }
